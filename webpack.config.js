cf5c2b3f80577ac43fe7a301831781fe00a67288
Chris Vandenberg <chrisvan0848@gmail.com>
2016-06-22 10:16:29 -0700

    updates to cinfig

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,8 @@
 const path = require('path');
 const args = require('minimist')(process.argv.slice(2));
 
-// List of allowed environments
 const allowedEnvs = ['dev', 'dist', 'test'];
 
-// Set the correct environment
 let env;
 if (args._.length > 0 && args._.indexOf('start') !== -1) {
   env = 'test';
@@ -17,11 +15,6 @@ if (args._.length > 0 && args._.indexOf('start') !== -1) {
 }
 process.env.REACT_WEBPACK_ENV = env;
 
-/**
- * Build the webpack configuration
- * @param  {String} wantedEnv The wanted environment
- * @return {Object} Webpack config
- */
 function buildConfig(wantedEnv) {
   let isValid = wantedEnv && wantedEnv.length > 0 && allowedEnvs.indexOf(wantedEnv) !== -1;
   let validEnv = isValid ? wantedEnv : 'dev';
