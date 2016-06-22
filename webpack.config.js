bfdb29e9b3cbf2242ecfac192a2c4f221522f057
Chris Vandenberg <chrisvan0848@gmail.com>
2016-06-22 11:53:05 -0700

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,22 @@
 const path = require('path')
 const args = require('minimist')(process.argv.slice(2))
 
-const allowedEnvs = ['dev', 'dist', 'test']
+const allowedEnvs = [`dev`, `dist`, `test`]
 
 let env
-if (args._.length > 0 && args._.indexOf('start') !== -1) {
-  env = 'test'
+if (args._.length > 0 && args._.indexOf(`start`) !== -1) {
+  env = `test`
 } else if (args.env) {
   env = args.env
 } else {
-  env = 'dev'
+  env = `dev`
 }
 process.env.REACT_WEBPACK_ENV = env
 
 function buildConfig(wantedEnv) {
   let isValid = wantedEnv && wantedEnv.length > 0 && allowedEnvs.indexOf(wantedEnv) !== -1
-  let validEnv = isValid ? wantedEnv : 'dev'
-  let config = require(path.join(__dirname, 'cfg/' + validEnv))
+  let validEnv = isValid ? wantedEnv : `dev`
+  let config = require(path.join(__dirname, `cfg/` + validEnv))
   return config
 }
 
